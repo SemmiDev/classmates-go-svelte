6a9386d30b244189f0dde68f584fdd183d56e159
SemmiDev <sammidev4@gmail.com>
2021-08-19 11:55:37 +0700

    backend

---
+++ classmates/go.mod
@@ -0,0 +1,5 @@
+module github.com/semmidev/classmates
+
+go 1.16
+
+require github.com/gofiber/fiber/v2 v2.17.0
---
+++ classmates/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+type Student struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Identifier string `json:"identifier"`
+	Email      string `json:"email"`
+	ImageURL   string `json:"image_url"`
+}
+
+var classmates = []Student {
+	{
+		ID:         1,
+		Name:       "sammi",
+		Identifier: "1010101010101",
+		Email:      "sammi@gmail.com",
+		ImageURL:   "https://raw.githubusercontent.com/SemmiDev/classmates-go-svelte/main/assets/sammi.png",
+	},
+	{
+		ID:         2,
+		Name:       "Rahmatul Izzah Annisa",
+		Identifier: "2020202020121",
+		Email:      "izzah@gmail.com",
+		ImageURL:   "https://raw.githubusercontent.com/izzaah/tugas-imk2/main/img/img/izzah.jpeg",
+	},
+	{
+		ID:         3,
+		Name:       "Ayatullah Ramadhan Jacoeb",
+		Identifier: "3030303030303",
+		Email:      "ayatullah@gmail.com",
+		ImageURL:   "https://raw.githubusercontent.com/izzaah/tugas-imk2/main/img/img/keluarga.png",
+	},
+}
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	data := struct {
+		Data []Student `json:"data"`
+	}{
+		classmates,
+	}
+
+	app.Get("/api/v1/classmates", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+		return c.Status(fiber.StatusOK).JSON(data)
+	})
+	app.Listen(":8080")
+}
